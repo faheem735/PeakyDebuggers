a04aef9bbd02e831697046e73730cf46a12090d9
PeakyDebuggers <52954957+PeakyDebuggers@users.noreply.github.com>
2019-08-20 17:50:15 +1000

    Update book.java

--- book.java
+++ book.java
@@ -22,14 +22,14 @@ public class Book implements Serializable {
 	}
 	
 	public String toString() {
-		StringBuilder stingBuilder = new StringBuilder();
-		sb.append("Book: ").append(bookId).append("\n")
-		  .append("  Title:  ").append(title).append("\n")
-		  .append("  Author: ").append(author).append("\n")
-		  .append("  phoneNO: ").append(phoneNo).append("\n")
-		  .append("  State:  ").append(State);
+		StringBuilder stingBuilder = new stingbuilder();
+		stingbuilder.append("Book: ").append(bookId).append("\n")
+		stingbuilder.append("  Title:  ").append(title).append("\n")
+		stingbuilder.append("  Author: ").append(author).append("\n")
+		stingbuilder.append("  phoneNO: ").append(phoneNo).append("\n")
+		stingbuilder.append("  State:  ").append(State);
 		
-		return sb.toString();
+		return stingbuilder.toString();
 	}
 
 	public Integer bookId() {// ID ro bookId
